fdd5fc30f8c6facdd035c4df37e3a70abda469bd
Juma13701 <jpa5637@psu.edu>
2020-08-25 20:33:42 +0000

    the code

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ print("Hello," +name+ "!")
 age = input("Enter your age:")
 age = int(age)
 
-print("you will be " + str((age+1)) + " next year.")
+print("You will be " + str((age+1)) + " next year.")
